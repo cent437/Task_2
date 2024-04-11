6485834474cfc6c9c7935c71dcebb33e5a893892
Эмир Ториев <torimir1977@yandex.ru>
2024-04-11 13:59:25 +0300

    Student3

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,13 +1,14 @@
 ﻿#include <iostream>
 int main()
 {
-<<<<<<< HEAD
-    int a2, b2=2, c2=3;
+
+    int a2, b2 = 2, c2 = 3;
     a2 = b2 + c2;
     std::cout << a2;
-=======
-    int a1, b1=2, c1=3;
-    a1 = b1 + c1;
-    std::cout << a1;
->>>>>>> 7bc5c4f41d377fffcb06158754d035d75a01ce2c
+
+    if (a2>0) // Проверка на положительность
+    {
+        std::cout << "a2 > 0";
+    }
 }
+  
